76dc0d48d1cd1d56d2c325b8ab54015fe5e43dab
Marcus Peterson <marcus.peterson@yh.nackademin.se>
2024-11-25 13:01:14 +0100

    setup

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     ],
     classifiers=[
         'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
+        'License :: OSI Approved :: Apache 2.0 License',
         'Operating System :: OS Independent',
     ],
     python_requires='>=3.6',
